1ff6efdd9df080acc3421489f7ea01aaf0979361
Wyatt J. Miller <wjmiller2016@gmail.com>
2020-06-13 15:06:46 -0400

    added configuration system

---
+++ src/config.rs
@@ -0,0 +1,34 @@
+use std::env;
+
+use config::File;
+use serde::{Deserialize};
+
+#[derive(Debug, Deserialize)]
+pub struct Configuration {
+    pub api_token: String,
+    pub base_api: String,
+    pub base_url: String,
+    pub username: Option<String>
+}
+
+impl Configuration {
+    pub fn new() -> Configuration {
+        let home_dir_env = env::var("HOME").unwrap();
+        let mut settings = config::Config::default();
+        let mut location: Vec<String> = Vec::new();
+        
+        location.push("config.json".to_string());
+        location.push("/etc/gt/config.json".to_string());
+        location.push(format!("{}/.config/gt/config.json", home_dir_env));
+
+        for i in location {
+            settings.merge(File::with_name(&i)
+                .required(false))
+                .unwrap();
+        }
+
+        let config = settings.try_into::<Configuration>().expect("Couldn't load config into gt!");
+
+        config
+    }
+}
